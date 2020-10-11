81dd565515d2724ade1734781ab69992a60aebd0
Jakub Plesnik <jakub.plesnik@sde.cz>
2020-10-11 18:55:00 +0200

    Add pimcore files

---
+++ var/config/predefined-asset-metadata.php
@@ -0,0 +1,28 @@
+<?php 
+
+return [
+    1 => [
+        "id" => 1,
+        "name" => "New Definition",
+        "description" => NULL,
+        "type" => "input",
+        "targetSubtype" => NULL,
+        "data" => NULL,
+        "config" => NULL,
+        "language" => NULL,
+        "creationDate" => 1602088393,
+        "modificationDate" => 1602088393
+    ],
+    2 => [
+        "id" => 2,
+        "name" => "New Definition",
+        "description" => NULL,
+        "type" => "input",
+        "targetSubtype" => NULL,
+        "data" => NULL,
+        "config" => NULL,
+        "language" => NULL,
+        "creationDate" => 1602088437,
+        "modificationDate" => 1602088437
+    ]
+];
