32f1c56d31d6c2d574bebfd4bd5f81eb2bba64b5
Jakub Plesnik <jakub.plesnik@sde.cz>
2020-10-12 16:03:13 +0200

    Add member package and sample template

---
+++ app/Resources/views/Default/test.html.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Test</title>
+</head>
+
+<body>
+
+
+<div id="site">
+    <div id="logo">
+        <h1>Test</h1>
+    </div>
+</div>
+
+</body>
+</html>
